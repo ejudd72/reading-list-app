02f0d4f082eb61d121a76772850b14511093d45b
Eleanor <e.e.j@live.co.uk>
2020-05-12 11:56:47 +0100

    Updates

---
+++ precache-manifest.344a7d2727d633624c27b28e6370cca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023c3580ada72b31856f7c28f4bcb65c",
+    "url": "/reading-list-app/index.html"
+  },
+  {
+    "revision": "d0d1d85fe5f47452906f",
+    "url": "/reading-list-app/static/css/main.e1d680f3.chunk.css"
+  },
+  {
+    "revision": "5c96777eea99c1bfaf82",
+    "url": "/reading-list-app/static/js/2.82975c2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reading-list-app/static/js/2.82975c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d1d85fe5f47452906f",
+    "url": "/reading-list-app/static/js/main.f083c659.chunk.js"
+  },
+  {
+    "revision": "b8d188420992b7bc2c5a",
+    "url": "/reading-list-app/static/js/runtime-main.bfe53067.js"
+  }
+]);
